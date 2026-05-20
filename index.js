ed828f4a13745e295f603998d6ba9e99d3ccc264
Mamun123123123 <almalm90459@gmail.com>
2026-05-20 10:55:12 +0600

    create booking api

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cors = require('cors')
 app.use(cors())
 app.use(express.json())
 const PORT = process.env.PORT
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = process.env.MONGODB_URI;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -23,7 +23,7 @@ async function run() {
 
     const db = client.db("drivefleet")
     const carCollection = db.collection("cars")
-   
+    const bookingCollection = db.collection("bookings")
     app.get('/explore-cars',async(req,res)=>{
         const result = await carCollection.find().toArray()
         res.json(result)
@@ -33,6 +33,19 @@ async function run() {
         const carData =  req.body
         const result = await carCollection.insertOne(carData)
         res.json(result)
+    })
+
+    app.get('/cars/:id',async(req,res)=>{
+        const {id} = req.params
+        const result = await carCollection.findOne({_id: new ObjectId(id)})
+        res.json(result)
+
+    })
+
+    app.post('/booking',async(req,res)=>{
+      const bookingData = req.body
+      const result = await bookingCollection.insertOne(bookingData)
+      res.json(result)
     })
 
 
