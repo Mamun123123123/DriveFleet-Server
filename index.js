04599312838572543527bc29181d862940291d3c
Mamun123123123 <almalm90459@gmail.com>
2026-05-20 15:26:50 +0600

    create put api

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ const app = express()
 const cors = require('cors')
 app.use(cors())
 app.use(express.json())
+
 const PORT = process.env.PORT
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
 const uri = process.env.MONGODB_URI;
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -16,74 +18,153 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   }
 });
+
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
+
     await client.connect();
 
     const db = client.db("drivefleet")
     const carCollection = db.collection("cars")
     const bookingCollection = db.collection("bookings")
-    app.get('/explore-cars',async(req,res)=>{
-        const result = await carCollection.find().toArray()
-        res.json(result)
+
+    app.get('/explore-cars', async (req, res) => {
+      const result = await carCollection.find().toArray()
+      res.json(result)
     })
 
-    app.post('/car' ,async(req,res)=>{
-        const carData =  req.body
-        const result = await carCollection.insertOne(carData)
-        res.json(result)
+    app.post('/car', async (req, res) => {
+      const carData = req.body
+      const result = await carCollection.insertOne(carData)
+      res.json(result)
     })
 
-    app.get('/cars/:id',async(req,res)=>{
-        const {id} = req.params
-        const result = await carCollection.findOne({_id: new ObjectId(id)})
-        res.json(result)
-
+    app.get('/cars/:id', async (req, res) => {
+      const { id } = req.params
+      const result = await carCollection.findOne({ _id: new ObjectId(id) })
+      res.json(result)
     })
 
-    app.post('/booking',async(req,res)=>{
+    app.post('/booking', async (req, res) => {
       const bookingData = req.body
       const result = await bookingCollection.insertOne(bookingData)
       res.json(result)
     })
 
-    app.get("/bookings/:userId",async(req,res)=>{
-          const {userId} = req.params
-          const result = await bookingCollection.find({userId:userId}).toArray()
-          res.json(result)
+    app.get("/bookings/:userId", async (req, res) => {
+      const { userId } = req.params
+      const result = await bookingCollection.find({ userId }).toArray()
+      res.json(result)
     })
 
+    app.get('/my-added-cars/:email', async (req, res) => {
 
+      try {
 
+        const { email } = req.params;
 
+        const result = await carCollection.find({ userEmail: email }).toArray();
 
+        res.json(result);
 
+      } catch (error) {
 
+        res.status(500).json({
+          success: false,
+          message: error.message
+        });
 
+      }
 
+    });
 
-    // Send a ping to confirm a successful connection
+    app.get('/car/:id', async (req, res) => {
+      try {
+        const { id } = req.params;
+
+        const result = await carCollection.findOne({
+          _id: new ObjectId(id)
+        });
+
+        res.json(result);
+
+      } catch (error) {
+        res.status(500).json({ message: error.message });
+      }
+    });
+
+    app.delete('/car/:id', async (req, res) => {
+      try {
+
+        const { id } = req.params;
+
+        const result = await carCollection.deleteOne({
+          _id: new ObjectId(id)
+        });
+
+        res.json(result);
+
+      } catch (error) {
+
+        res.status(500).json({
+          success: false,
+          message: error.message
+        });
+
+      }
+    });
+
+   
+    app.put('/car/:id', async (req, res) => {
+
+      try {
+
+        const { id } = req.params;
+        const updatedCar = req.body;
+
+        const result = await carCollection.updateOne(
+          { _id: new ObjectId(id) },
+          {
+            $set: {
+              carName: updatedCar.carName,
+              dailyRentPrice: updatedCar.dailyRentPrice,
+              carType: updatedCar.carType,
+              imageUrl: updatedCar.imageUrl,
+              seatCapacity: updatedCar.seatCapacity,
+              pickupLocation: updatedCar.pickupLocation,
+              description: updatedCar.description,
+              availabilityStatus: updatedCar.availabilityStatus
+            }
+          }
+        );
+
+        res.json(result);
+
+      } catch (error) {
+
+        res.status(500).json({
+          success: false,
+          message: error.message
+        });
+
+      }
+
+    });
+
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log("Connected to MongoDB");
+
   } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
+    
   }
 }
+
 run().catch(console.dir);
 
-
-app.get('/',(req,res)=>{
-    res.send("Server is running...")
+app.get('/', (req, res) => {
+  res.send("Server is running...")
 })
 
-
-
-
-
-
-app.listen(PORT,()=>{
-    console.log(`Server running on port ${PORT}`);
-    
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 })
