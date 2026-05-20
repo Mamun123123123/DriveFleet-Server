e10df59075e858e6d6fb48229e05ebaa2c518549
Mamun123123123 <almalm90459@gmail.com>
2026-05-20 23:50:54 +0600

    all done

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const client = new MongoClient(uri, {
 });
 
 const JWKS = createRemoteJWKSet(
-  new URL("http://localhost:3000/api/auth/jwks")
+  new URL(`${process.env.CLIENT_URL}/api/auth/jwks`)
 )
 
 const verifyToken = async(req,res,next)=>{
@@ -49,7 +49,7 @@ const verifyToken = async(req,res,next)=>{
 async function run() {
   try {
 
-    await client.connect();
+    // await client.connect();
 
     const db = client.db("drivefleet")
     const carCollection = db.collection("cars")
@@ -218,7 +218,7 @@ async function run() {
 
     });
 
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Connected to MongoDB");
 
   } finally {
